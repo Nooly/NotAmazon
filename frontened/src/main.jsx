2dca8b88cb9b7687eed249e86490e5316a3ea88a
Nooly <NoolyTheCat@gmail.com>
2024-03-07 11:41:10 +0200

    update url front

--- frontened/src/main.jsx
+++ frontened/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './index.css'
 import { StoreProvider } from './Store.jsx';
 
-axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://not-amazon-sooty.vercel.app/';
+axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://not-amazon.vercel.app/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
