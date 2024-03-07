75339deba263ee8c8d88e3f7208ea0dd54b34f60
Nooly <NoolyTheCat@gmail.com>
2024-03-07 11:33:17 +0200

    added url for new server

--- frontened/src/main.jsx
+++ frontened/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './index.css'
 import { StoreProvider } from './Store.jsx';
 
-axios.defaults.baseURL = 'http://localhost:8080'; // Change this to server once we have one in production or import from .env
+axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://not-amazon-sooty.vercel.app/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
